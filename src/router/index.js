80e31e60308763dd29d468e4796c0159c99f3101
pwh-pwh <plmoknpwh@163.com>
2022-01-23 21:36:19 +0800

    增加welcome子路由

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: () => import('../views/Admin'),
+    children: [
+      {
+        path: 'welcome', //注意这里不能加斜杠
+        component: () => import('../views/admin/Welcome')
+      }
+    ]
   }
 ]
 
---
+++ src/views/admin/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+<div id="welcome">
+  <h2>
+    欢迎来到这里
+  </h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Welcome"
+}
+</script>
+
+<style scoped>
+
+</style>
